0d950f25fa5c4e589afed80f42946dbb97bf913c
PALR-1216 <palr12162000@gmail.com>
2022-09-25 18:09:28 -0400

    fd

--- index.js
+++ index.js
@@ -28,24 +28,24 @@ app.use(bodyParser.urlencoded({extended:true}))
 
 
 //Connect Database for local develompent im MAMP
-let conn = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"root",
-    database:"myWorkTracker",
-    port:8889
-})
+// let conn = mysql.createConnection({
+//     host:"localhost",
+//     user:"root",
+//     password:"root",
+//     database:"myWorkTracker",
+//     port:8889
+// })
 
 
 //Connect Database for Global Develompent in cleverCloud
-// let conn = mysql.createConnection({
-//     host:"bp2qffuyks8vhungamgb-mysql.services.clever-cloud.com",
-//     user:"u0mtfkpltlezrzpw",
-//     password:"8UwCtVlwGUFA17fVqfjK",
-//     port:3306,
-//     database:"bp2qffuyks8vhungamgb"
+let conn = mysql.createConnection({
+    host:"bp2qffuyks8vhungamgb-mysql.services.clever-cloud.com",
+    user:"u0mtfkpltlezrzpw",
+    password:"8UwCtVlwGUFA17fVqfjK",
+    port:3306,
+    database:"bp2qffuyks8vhungamgb"
 
-// })
+})
 
 
 conn.connect((err) =>{
